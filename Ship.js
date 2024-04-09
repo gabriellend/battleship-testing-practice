b346e64b00b8303562f18a4cbb32c54cebcaea63
gabriellend <gdvdsn@gmail.com>
2024-04-08 18:37:44 -1000

    Handle errors and make isSunk a getter

--- Ship.js
+++ Ship.js
@@ -3,15 +3,22 @@ export class Ship {
   #numHits;
 
   constructor(length) {
+    if (!Number.isInteger(length) || length <= 0) {
+      throw new Error("Length must be a positive integer.");
+    }
     this.#length = length;
     this.#numHits = 0;
   }
 
   hit() {
+    if (this.isSunk) {
+      throw new Error("Ship is already sunk!");
+    }
+
     this.#numHits++;
   }
 
-  isSunk() {
+  get isSunk() {
     return this.#length === this.#numHits;
   }
 }
